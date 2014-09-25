28947a1128c73765d795cf1dd0fcc1bddeb3a086
Arne Neumann <github@arne.cl>
2014-09-25 15:20:00 +0200

    minor: formatting

--- spectrum_kernel.py
+++ spectrum_kernel.py
@@ -7,15 +7,15 @@
 def k_suffix_kernel(k, s, t):
     """
     calculates k-suffix kernel of input strings s and t::
-    
+
         K_{k}^{S}(s,t)=
         \begin{cases}
         1 \text{ if } s=s_{1}u, t=t_{1}u, \text{ for } u \in \Sigma^{k} & \\
         0 \textrm{ otherwise}
         \end{cases}
-        
+
         (Shawe-Taylor and Cristianini 2004, p.349)
-    
+
     Parameters
     ----------
     k : int
@@ -24,7 +24,7 @@ def k_suffix_kernel(k, s, t):
         input string 1
     t : str
         input string 2
-    
+
     Returns
     -------
     product : int
@@ -32,25 +32,27 @@ def k_suffix_kernel(k, s, t):
         otherwise, returns 0.
     """
     assert min(len(s), len(t)) >= k, \
-		"strings must be at least as long as the given suffix length k"
+        "strings must be at least as long as the given suffix length k"
     s_suffix = s[-k:] # suffix of length k
     t_suffix = t[-k:]
-    return 1 if s_suffix == t_suffix else 0    
+    return 1 if s_suffix == t_suffix else 0
 
 
 def p_spectrum_kernel(p, s, t):
     """
     calculates the inner product of the p-spectra of
     the strings s and t.
-    
+
     The p-spectrum of a sequence is defined as the
     histogram of frequencies of all its (contiguous)
     substrings of length p::
-    
-        \sum\limits^{|s|-p+1}_{i=1} \sum\limits^{|t|-p+1}_{j=1} K^{S}_{p}(s(i:i+p), t(j:j+p))
-    
+
+        \sum\limits^{|s|-p+1}_{i=1}
+            \sum\limits^{|t|-p+1}_{j=1}
+                K^{S}_{p}(s(i:i+p), t(j:j+p))
+
         (Shawe-Taylor and Cristianini 2004, p.349)
-    
+
     Paramters
     ---------
     p : int
@@ -60,11 +62,11 @@ def p_spectrum_kernel(p, s, t):
     t : str
         input string 2
 
-	Returns
-	-------
-	product : int
-		returns the number of p-length contiguous substrings
-		that both input strings have in common       
+    Returns
+    -------
+    product : int
+        returns the number of p-length contiguous substrings
+        that both input strings have in common
     """
     result = 0
     for i in xrange(len(s)-p+1):
