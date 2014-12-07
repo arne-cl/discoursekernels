4b37c891e6bc5e693cb1c96a126064898a26c765
Arne Neumann <github@arne.cl>
2014-12-07 23:07:50 +0100

    tree kernel: added more test trees

--- test_tree.py
+++ test_tree.py
@@ -1,4 +1,5 @@
 
+import networkx as nx
 from networkx import DiGraph
 
 def example_tree():
@@ -68,3 +69,125 @@ def test_is_subtree():
         assert is_subtree(tree, valid_subtree) == True
     for invalid_subtree in invalid_subtrees:
         assert is_subtree(tree, invalid_subtree) == False
+
+def label_nodes(node_label_tuples_list):
+    """
+    convert a list of (node ID, node label) tuples into a list of
+    (node ID, {'label': node label}) tuples, which can be added to a
+    networkx graph via .add_nodes_from().
+    """
+    nodes = []
+    for node_id, node_label in node_label_tuples_list:
+        nodes.append( (node_id, {'label': node_label}) )
+    return nodes
+
+
+# Example trees
+
+## Jeff ate cookies
+
+tree_jeff_ate_cookies = nx.DiGraph()
+tree_jeff_ate_cookies.add_nodes_from(label_nodes([
+    (1, 'S'),
+    (2, 'NP'), (3, 'N'), (4, 'Jeff'),
+    (5, 'VP'), (6, 'V'), (7, 'ate'),
+    (8, 'NP'), (9, 'N'), (10, 'cookies')]))
+tree_jeff_ate_cookies.add_edges_from([
+    (1, 2), # S NP
+    (1, 5), # S VP
+    (2, 3), # NP N
+    (3, 4), # N Jeff
+    (5, 6), # VP V
+    (6, 7), # V ate
+    (5, 8), # VP NP
+    (8, 9), # NP N
+    (9, 10), # N cookies
+])
+
+
+## Steve ate bananas
+
+tree_steve_ate_bananas = nx.DiGraph()
+tree_steve_ate_bananas.add_nodes_from(label_nodes([
+    (1, 'S'),
+    (2, 'NP'), (3, 'N'), (4, 'Steve'),
+    (5, 'VP'), (6, 'V'), (7, 'ate'),
+    (8, 'NP'), (9, 'N'), (10, 'bananas')]))
+tree_steve_ate_bananas.add_edges_from([
+    (1, 2), # S NP
+    (1, 5), # S VP
+    (2, 3), # NP N
+    (3, 4), # N Steve
+    (5, 6), # VP V
+    (6, 7), # V ate
+    (5, 8), # VP NP
+    (8, 9), # NP N
+    (9, 10), # N bananas
+])
+
+
+# Alex died
+
+tree_alex_died = nx.DiGraph()
+tree_alex_died.add_nodes_from(label_nodes([
+    (1, 'S'),
+    (2, 'NP'), (3, 'N'), (4, 'Alex'),
+    (5, 'VP'), (6, 'V'), (7, 'died')]))
+tree_alex_died.add_edges_from([
+    (1, 2), # S NP
+    (1, 5), # S VP
+    (2, 3), # NP N
+    (3, 4), # N Alex
+    (5, 6), # VP V
+    (6, 7), # V died
+])
+
+
+# The man drank wine
+
+
+tree_the_man_drank_wine = nx.DiGraph()
+tree_the_man_drank_wine.add_nodes_from(label_nodes([
+    (1, 'S'),
+    (2, 'NP'), (3, 'D'), (4, 'the'), (5, 'N'), (6, 'man'),
+    (7, 'VP'), (8, 'V'), (9, 'drank'),
+    (10, 'NP'), (11, 'N'), (12, 'wine')]))
+tree_the_man_drank_wine.add_edges_from([
+    (1, 2), # S NP
+    (1, 7), # S VP
+    (2, 3), # NP D
+    (2, 5), # NP N
+    (3, 4), # D the
+    (5, 6), # N man
+    (7, 8), # VP V
+    (8, 9), # V drank
+    (7, 10), # V NP
+    (10, 11), # NP N
+    (11, 12), # N wine
+])
+
+
+# The man killed the woman
+
+
+tree_the_man_killed_the_woman = nx.DiGraph()
+tree_the_man_killed_the_woman.add_nodes_from(label_nodes([
+    (1, 'S'),
+    (2, 'NP'), (3, 'D'), (4, 'the'), (5, 'N'), (6, 'man'),
+    (7, 'VP'), (8, 'V'), (9, 'killed'),
+    (10, 'NP'), (11, 'D'), (12, 'the'), (13, 'N'), (14, 'woman')]))
+tree_the_man_killed_the_woman.add_edges_from([
+    (1, 2), # S NP
+    (1, 7), # S VP
+    (2, 3), # NP D
+    (2, 5), # NP N
+    (3, 4), # D the
+    (5, 6), # N man
+    (7, 8), # VP V
+    (8, 9), # V killed
+    (7, 10), # V NP
+    (10, 11), # NP D
+    (10, 13), # NP N
+    (11, 12), # D the
+    (13, 14), # N woman
+])
